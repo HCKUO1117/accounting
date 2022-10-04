4066fa2798571ac7491f8dbb676631100cd1b4af
james <james1117pig@gmail.com>
2022-10-04 08:55:47 +0800

    style change

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -49,7 +49,8 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                 CustomDatePickerDialog.show(
                   context,
                   onDateSelect: (arg) {
-                    provider.setDashBoardDateRange(arg.value as PickerDateRange);
+                    provider
+                        .setDashBoardDateRange(arg.value as PickerDateRange);
                   },
                   start: provider.dashBoardStartDate,
                   end: provider.dashBoardEndDate,
@@ -85,7 +86,12 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                     color: AppColors.backgroundColor,
                     child: ConstrainedBox(
                       constraints: BoxConstraints(
-                        minHeight: MediaQuery.of(context).size.height / 3 * 2 + 0,
+                        minHeight: MediaQuery.of(context).size.height -
+                            90 -
+                            (MediaQuery.of(context).padding.top +
+                                kToolbarHeight +
+                                MediaQuery.of(context).padding.bottom +
+                                kBottomNavigationBarHeight),
                       ),
                       child: Container(
                         padding: const EdgeInsets.all(16),
@@ -114,9 +120,11 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                                   isScrollControlled: true,
                                   context: context,
                                   builder: (context) => Padding(
-                                    padding: EdgeInsets.only(top: widget.topPadding),
+                                    padding:
+                                        EdgeInsets.only(top: widget.topPadding),
                                     child: AddRecodePage(
-                                      model: provider.currentAccountingList[index],
+                                      model:
+                                          provider.currentAccountingList[index],
                                     ),
                                   ),
                                 );
@@ -156,7 +164,8 @@ class _PieData {
   final Color color;
 }
 
-class DashBoardSliverPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
+class DashBoardSliverPersistentHeaderDelegate
+    extends SliverPersistentHeaderDelegate {
   final double maxEx;
   final double minEx;
 
@@ -166,7 +175,8 @@ class DashBoardSliverPersistentHeaderDelegate extends SliverPersistentHeaderDele
   });
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
     double shrinkPercentage = min(1, shrinkOffset / (maxExtent - minExtent));
     return Consumer<MainProvider>(
       builder: (BuildContext context, MainProvider provider, _) {
@@ -208,7 +218,8 @@ class DashBoardSliverPersistentHeaderDelegate extends SliverPersistentHeaderDele
                           child: Column(
                             children: [
                               Container(
-                                margin: const EdgeInsets.symmetric(horizontal: 16),
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 16),
                                 padding: const EdgeInsets.all(8),
                                 decoration: BoxDecoration(
                                     color: Colors.white,
@@ -257,7 +268,8 @@ class DashBoardSliverPersistentHeaderDelegate extends SliverPersistentHeaderDele
                                           ),
                                           const SizedBox(height: 8),
                                           Text(
-                                            provider.currentExpenditure.toString(),
+                                            provider.currentExpenditure
+                                                .toString(),
                                             style: const TextStyle(
                                               color: Colors.redAccent,
                                               fontSize: 18,
@@ -280,26 +292,37 @@ class DashBoardSliverPersistentHeaderDelegate extends SliverPersistentHeaderDele
                                         explode: true,
                                         explodeIndex: 0,
                                         dataSource: [
-                                          if (provider.allEmpty || provider.currentExpenditure != 0)
+                                          if (provider.allEmpty ||
+                                              provider.currentExpenditure != 0)
                                             _PieData(
                                               S.of(context).expenditure,
-                                              provider.allEmpty ? 1 : provider.currentExpenditure,
+                                              provider.allEmpty
+                                                  ? 1
+                                                  : provider.currentExpenditure,
                                               S.of(context).expenditure,
                                               Colors.redAccent.shade100,
                                             ),
-                                          if (provider.allEmpty || provider.currentIncome != 0)
+                                          if (provider.allEmpty ||
+                                              provider.currentIncome != 0)
                                             _PieData(
                                               S.of(context).income,
-                                              provider.allEmpty ? 1 : provider.currentIncome,
+                                              provider.allEmpty
+                                                  ? 1
+                                                  : provider.currentIncome,
                                               S.of(context).income,
                                               Colors.blueAccent.shade100,
                                             ),
                                         ],
-                                        xValueMapper: (_PieData data, _) => data.xData,
-                                        yValueMapper: (_PieData data, _) => data.yData,
-                                        dataLabelMapper: (_PieData data, _) => data.text,
-                                        pointColorMapper: (_PieData data, _) => data.color,
-                                        dataLabelSettings: const DataLabelSettings(
+                                        xValueMapper: (_PieData data, _) =>
+                                            data.xData,
+                                        yValueMapper: (_PieData data, _) =>
+                                            data.yData,
+                                        dataLabelMapper: (_PieData data, _) =>
+                                            data.text,
+                                        pointColorMapper: (_PieData data, _) =>
+                                            data.color,
+                                        dataLabelSettings:
+                                            const DataLabelSettings(
                                           isVisible: true,
                                           textStyle: TextStyle(
                                             fontFamily: 'RobotoMono',
@@ -387,5 +410,6 @@ class DashBoardSliverPersistentHeaderDelegate extends SliverPersistentHeaderDele
   double get minExtent => minEx;
 
   @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true; // 如果内容需要更新，设置为true
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) =>
+      true; // 如果内容需要更新，设置为true
 }
